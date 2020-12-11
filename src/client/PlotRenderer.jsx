ef093c4689dec18b4320e4ab35af3fa222836e10
ShmuelLotman <shmuel.lotman@gmail.com>
2020-12-10 20:18:52 -0600

    feat: updates

--- src/client/PlotRenderer.jsx
+++ src/client/PlotRenderer.jsx
@@ -1,191 +1,210 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { Plot, newTable, geoTable, fromFlux } from "@influxdata/giraffe";
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Plot, newTable, geoTable, fromFlux } from '@influxdata/giraffe'
 const style = {
-  width: "calc(70vw - 20px)",
-  height: "calc(70vh - 20px)",
-  margin: "40px",
-};
-
-const REASONABLE_API_REFRESH_RATE = 30000;
-
+  width: 'calc(70vw - 20px)',
+  height: 'calc(70vh - 20px)',
+  margin: '40px',
+}
+const REASONABLE_API_REFRESH_RATE = 30000
 export class PlotRenderer extends React.Component {
   constructor(props) {
-    super(props);
-
-    this.animationFrameId;
-
+    super(props)
+    this.animationFrameId
     this.state = {
       layer: {
-        type: "geo",
+        type: 'geo',
         lat: 40,
-        lon: -76,
+        lon: 76,
         zoom: 6,
         allowPanAndZoom: true,
         detectCoordinateFields: false,
         layers: [
           {
-            type: "pointMap",
-            colorDimension: { label: "Duration" },
-            colorField: "duration",
+            type: 'pointMap',
+            colorDimension: { label: 'Duration' },
+            colorField: 'duration',
             colors: [
-              { type: "min", hex: "#ff0000" },
-              { value: 50, hex: "#343aeb" },
-              { type: "max", hex: "#343aeb" },
+              { type: 'min', hex: '#ff0000' },
+              { value: 50, hex: '#343aeb' },
+              { type: 'max', hex: '#343aeb' },
             ],
             isClustered: false,
           },
         ],
-        tileServerConfiguration: { tileServerUrl: "", bingKey: "" },
+        tileServerConfiguration: {
+          tileServerUrl: `http://localhost:8617/map?z=1&x=40&y=76`,
+          bingKey: '',
+        },
       },
       table: {},
       timestamps: [],
       values: [],
-    };
-
-    this.animateFakeData = this.animateFakeData.bind(this);
-    this.animateRealData = this.animateRealData.bind(this);
-    this.createFakeDataTable = this.createFakeDataTable.bind(this);
-    this.createRealDataTable = this.createRealDataTable.bind(this);
-    this.getMapUrlKey = this.getMapUrlKey.bind(this);
+    }
+    this.animateFakeData = this.animateFakeData.bind(this)
+    this.animateRealData = this.animateRealData.bind(this)
+    this.createFakeDataTable = this.createFakeDataTable.bind(this)
+    this.createRealDataTable = this.createRealDataTable.bind(this)
+    //  this.getMapUrlKey = this.getMapUrlKey.bind(this)
     // this.getMap = this.getMap.bind(this);
-    this.fetchData = this.fetchData.bind(this);
+    this.fetchData = this.fetchData.bind(this)
   }
-
   async componentDidMount() {
     try {
-      this.createRealDataTable();
-      await this.getMapUrlKey();
-      // this.getMap();
+      this.createRealDataTable()
+      //await this.getMapUrlKey()
+      //this.getMap()
       this.animationFrameId = window.setInterval(
         this.animateRealData,
         REASONABLE_API_REFRESH_RATE
-      );
+      )
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   }
-
   componentWillUnmount() {
-    window.clearInterval(this.animationFrameId);
+    window.clearInterval(this.animationFrameId)
   }
-
   render() {
     const config = {
       table: this.state.table,
       layers: [this.state.layer],
-    };
-
-    if (!Object.keys(config.table).length) {
-      return null;
     }
-
+    if (!Object.keys(config.table).length) {
+      return null
+    }
     return (
       <div style={style}>
         <Plot config={config} />
       </div>
-    );
+    )
   }
-
   fetchData() {
-    return fetch("http://localhost:8617/query", {
+    return fetch('http://localhost:8617/query', {
       headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
-    });
+    })
   }
-
   animateRealData() {
-    this.createRealDataTable();
+    this.createRealDataTable()
   }
-
   animateFakeData() {
-    const lastTimestamp = [...this.state.timestamps].pop();
-    const nextTimestamp = lastTimestamp + 6000;
-
-    const timestamps = [...this.state.timestamps, nextTimestamp];
+    const lastTimestamp = [...this.state.timestamps].pop()
+    const nextTimestamp = lastTimestamp + 6000
+    const timestamps = [...this.state.timestamps, nextTimestamp]
     const values = [
       ...this.state.values,
       window.parseFloat(Math.random() * (10 - 1) + 1),
-    ];
-
+    ]
     if (timestamps.length > 50) {
-      console.log("removing older data");
-      timestamps.shift();
-      values.shift();
+      console.log('removing older data')
+      timestamps.shift()
+      values.shift()
     }
-
     this.setState({
       timestamps,
       values,
-    });
+    })
   }
-
   createFakeDataTable() {
     return newTable(this.state.timestamps.length)
-      .addColumn("_time", "dateTime:RFC3339", "time", this.state.timestamps)
-      .addColumn("_value", "double", "number", this.state.values);
+      .addColumn('_time', 'dateTime:RFC3339', 'time', this.state.timestamps)
+      .addColumn('_value', 'double', 'number', this.state.values)
   }
-
   async createRealDataTable() {
-    const resp = await this.fetchData();
-    const resultsCSV = await resp.text();
-    let results;
-
+    const resp = await this.fetchData()
+    const resultsCSV = await resp.text()
+    let results
     try {
-      results = fromFlux(resultsCSV);
+      results = fromFlux(resultsCSV)
     } catch (error) {
-      console.error("error", error.message);
+      console.error('error', error.message)
     }
-
-    console.log("results: ", results);
+    console.log('results: ', results)
     this.setState({
       table: results.table,
-    });
+    })
+  }
+  // fetchMapUrlKey() {
+  //   return fetch('http://localhost:8617/apiUrlKey', {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Access-Control-Allow-Origin': '*',
+  //     },
+  //   })
+  // }
+  // async getMapUrlKey() {
+  //   const resp = await this.fetchMapUrlKey()
+  //   const res = await resp.json()
+  //   const { url, key } = res
+  //   const tileServerConfiguration = {
+  //     tileServerUrl: url + '?access_token=' + key,
+  //     bingKey: '',
+  //   }
+  //   this.setState({
+  //     layer: {
+  //       ...this.state.layer,
+  //       tileServerConfiguration: tileServerConfiguration,
+  //     },
+  //   })
+  //   return resp
+  // }
+  fetchMapData() {
+    return fetch(`http://localhost:8617/map?z=1&x=40&y=-76`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+  }
+  async getMap() {
+    console.log('I am in createrealdatatable')
+    const resp = await this.fetchMapData()
+    const res = await resp.json()
+    console.log('This is response', res)
   }
 
   fetchMapUrlKey() {
-    return fetch("http://localhost:8617/apiUrlKey", {
+    return fetch('http://localhost:8617/apiUrlKey', {
       headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
-    });
+    })
   }
 
   async getMapUrlKey() {
-    const resp = await this.fetchMapUrlKey();
-    const res = await resp.json();
-    const { url, key } = res;
+    const resp = await this.fetchMapUrlKey()
+    const res = await resp.json()
+    const { url, key } = res
     const tileServerConfiguration = {
-      tileServerUrl: url + "?access_token=" + key,
-      bingKey: "",
-    };
+      tileServerUrl: url + '?access_token=' + key,
+      bingKey: '',
+    }
     this.setState({
       layer: {
         ...this.state.layer,
         tileServerConfiguration: tileServerConfiguration,
       },
-    });
-    return resp;
+    })
+    return resp
   }
 
   fetchMapData() {
-    return fetch("http://localhost:8617/map", {
+    return fetch('http://localhost:8617/map', {
       headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
-    });
+    })
   }
 
   async getMap() {
-    console.log("I am in createrealdatatable");
-    const resp = await this.fetchMapData();
-    const res = await resp.json();
-    console.log("This is response", res);
+    console.log('I am in createrealdatatable')
+    const resp = await this.fetchMapData()
+    const res = await resp.json()
+    console.log('This is response', res)
   }
 }
--- src/server/main.js
+++ src/server/main.js
@@ -1,78 +1,129 @@
 #!/usr/bin/env node
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
-
 import axios from 'axios'
 import moment from 'moment'
-
-const baseURL = process.env.INFLUX_URL; // url of your cloud instance (e.g. https://us-west-2-1.aws.cloud2.influxdata.com/)
-const influxToken = process.env.INFLUX_TOKEN; // create an all access token in the UI, export it as INFLUX_TOKEN
-const orgID = process.env.ORG_ID; // export your org id;
-const mapboxUrl = process.env.API_URL;
-const apiKey = process.env.API_KEY; //export your own apiKey;
-
+const baseURL = process.env.INFLUX_URL // url of your cloud instance (e.g. https://us-west-2-1.aws.cloud2.influxdata.com/)
+const influxToken = process.env.INFLUX_TOKEN // create an all access token in the UI, export it as INFLUX_TOKEN
+const orgID = process.env.ORG_ID // export your org id;
+const mapboxUrl = process.env.API_URL
+const API_KEY = process.env.API_KEY //export your own apiKey;
 const influxProxy = axios.create({
   baseURL,
   headers: {
-    'Authorization': `Token ${influxToken}`,
-    'Content-Type': 'application/json'
-  }
-});
-
-const app = express();
-const port = 8617;
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: './' });
-});
-
-app.get('/dist/bundle.js', (req, res) => {
-  res.sendFile('bundle.js', { root: './dist' });
+    Authorization: `Token ${influxToken}`,
+    'Content-Type': 'application/json',
+  },
 })
-
+const app = express()
+const port = 8617
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: './' })
+})
+app.get('/dist/bundle.js', (req, res) => {
+  res.sendFile('bundle.js', { root: './dist' })
+})
 app.get('/query', (req, res) => {
-  const bucket = 'telegraf';
+  const bucket = 'telegraf'
   const start = '2019-02-01 00:00:00.000'
   const stop = '2020-02-28 23:59:00.000'
-
   const query = `
-  from(bucket: "palak+cloud2's Bucket")
-  |> range(start: ${moment(start).toISOString()}, stop: ${moment(stop).toISOString()})
+  from(bucket: "slotman's Bucket")
+  |> range(start: ${moment(start).toISOString()}, stop: ${moment(
+    stop
+  ).toISOString()})
   |> filter(fn: (r) => r["_measurement"] == "migration")
   |> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: false)
   |> yield(name: "mean")
-  `.trim();
-
-  influxProxy.request({
-    method: 'post',
-    url: 'api/v2/query',
-    params: {
-      orgID
-    },
-    data: {
-      query,
-      extern: {"type":"File","package":null,"imports":null,"body":[{"type":"OptionStatement","assignment":{"type":"VariableAssignment","id":{"type":"Identifier","name":"v"},"init":{"type":"ObjectExpression","properties":[{"type":"Property","key":{"type":"Identifier","name":"bucket"},"value":{"type":"StringLiteral","value":"telegraf"}},{"type":"Property","key":{"type":"Identifier","name":"timeRangeStart"},"value":{"type":"UnaryExpression","operator":"-","argument":{"type":"DurationLiteral","values":[{"magnitude":1,"unit":"h"}]}}},{"type":"Property","key":{"type":"Identifier","name":"timeRangeStop"},"value":{"type":"CallExpression","callee":{"type":"Identifier","name":"now"}}},{"type":"Property","key":{"type":"Identifier","name":"windowPeriod"},"value":{"type":"DurationLiteral","values":[{"magnitude":10000,"unit":"ms"}]}}]}}}]},
-      dialect :{"annotations":["group","datatype","default"]}
-    }
-  }).then((response) => {
-    res.send(response.data)
-  }).catch(error => {
-    res.send(error.message)
-  });
-
-})
-
-app.get('/apiUrlKey', (req, res) => {
-  res.send({ url: mapboxUrl, key: apiKey })
-  })
-
-app.get('/map', (req, res) => {
-  console.log("maboxUrls", mapboxUrl);
-  const link = mapboxUrl + '?access_token=' + apiKey;
-  axios.get(link).then((response) => {
+  `.trim()
+  influxProxy
+    .request({
+      method: 'post',
+      url: 'api/v2/query',
+      params: {
+        orgID,
+      },
+      data: {
+        query,
+        extern: {
+          type: 'File',
+          package: null,
+          imports: null,
+          body: [
+            {
+              type: 'OptionStatement',
+              assignment: {
+                type: 'VariableAssignment',
+                id: { type: 'Identifier', name: 'v' },
+                init: {
+                  type: 'ObjectExpression',
+                  properties: [
+                    {
+                      type: 'Property',
+                      key: { type: 'Identifier', name: 'bucket' },
+                      value: { type: 'StringLiteral', value: 'telegraf' },
+                    },
+                    {
+                      type: 'Property',
+                      key: { type: 'Identifier', name: 'timeRangeStart' },
+                      value: {
+                        type: 'UnaryExpression',
+                        operator: '-',
+                        argument: {
+                          type: 'DurationLiteral',
+                          values: [{ magnitude: 1, unit: 'h' }],
+                        },
+                      },
+                    },
+                    {
+                      type: 'Property',
+                      key: { type: 'Identifier', name: 'timeRangeStop' },
+                      value: {
+                        type: 'CallExpression',
+                        callee: { type: 'Identifier', name: 'now' },
+                      },
+                    },
+                    {
+                      type: 'Property',
+                      key: { type: 'Identifier', name: 'windowPeriod' },
+                      value: {
+                        type: 'DurationLiteral',
+                        values: [{ magnitude: 10000, unit: 'ms' }],
+                      },
+                    },
+                  ],
+                },
+              },
+            },
+          ],
+        },
+        dialect: { annotations: ['group', 'datatype', 'default'] },
+      },
+    })
+    .then((response) => {
       res.send(response.data)
     })
+    .catch((error) => {
+      res.send(error.message)
+    })
 })
-
+app.get('/apiUrlKey', (req, res) => {
+  res.send({ url: mapboxUrl, key: API_KEY })
+})
+app.get('/map', (req, res) => {
+  const { x, y, z } = req.query
+  console.log('maboxUrls', mapboxUrl, x, y, z)
+  const link = `${mapboxUrl}/${z}/${x}/${y}@2x?access_token=${API_KEY}`
+  axios
+    .get(link)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 app.listen(port, () => {
-  console.log(`listening on port :${port}`);
-});
+  console.log(`listening on port :${port}`)
+})
